92f3d977cdae9d51132ae23fc58b90b463c19d91
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-05 21:59:54 +0100

    Added zombieHorde

---
+++ ex01/zombieHorde.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 17:16:11 by abenamar          #+#    #+#             */
+/*   Updated: 2023/12/05 21:26:50 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie *zombieHorde(int N, std::string name)
+{
+	Zombie *zombies;
+
+	zombies = new Zombie[N];
+
+	for (int i = 0; i < N; ++i)
+	{
+		zombies[i].setName(name);
+	}
+
+	return zombies;
+}
