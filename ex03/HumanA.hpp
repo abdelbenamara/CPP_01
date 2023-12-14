33c9cf46597deb8f9bb607e03a24c69f38b48a88
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-14 13:16:48 +0100

    Updated HumanA

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,12 +6,12 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 23:45:17 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/06 00:18:53 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/14 13:09:03 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HUMANA_HPP
-#define HUMANA_HPP
+#ifndef __HUMANA_HPP__
+#define __HUMANA_HPP__
 
 #include <iostream>
 #include "Weapon.hpp"
@@ -20,9 +20,9 @@ class HumanA
 {
 public:
 	HumanA(std::string name, Weapon &weapon);
-	~HumanA();
+	~HumanA(void);
 
-	void attack() const;
+	void attack(void) const;
 
 private:
 	std::string name;
