d4866730bedd288546f76463599a5816dc412a4d
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-14 13:13:32 +0100

    Updated Zombie

--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -6,12 +6,12 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 11:47:29 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/05 17:18:23 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/14 13:06:32 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-#define ZOMBIE_HPP
+#ifndef __ZOMBIE_HPP__
+#define __ZOMBIE_HPP__
 
 #include <iostream>
 #include <string>
@@ -19,11 +19,11 @@
 class Zombie
 {
 public:
-	Zombie();
-	~Zombie();
+	Zombie(void);
+	~Zombie(void);
 
 	void setName(std::string name);
-	void announce() const;
+	void announce(void) const;
 
 private:
 	std::string name;
