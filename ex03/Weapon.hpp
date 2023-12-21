b6acefe34b07f62e0f28145883c223589c2bdc15
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-21 14:26:40 +0100

    Updated Weapon

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 23:40:18 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/14 13:08:52 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/21 14:08:18 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 class Weapon
 {
 public:
-	Weapon(std::string type);
+	Weapon(std::string const &type);
 	~Weapon(void);
 
-	std::string getType(void) const;
-	void setType(std::string type);
+	std::string const &getType(void) const;
+	void setType(std::string const &type);
 
 private:
 	std::string type;
