742e6e5ff788d29549a8f576f8a185d8de4ee0f1
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-21 14:26:58 +0100

    Updated Weapon

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 23:42:58 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/14 13:08:58 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/21 14:08:27 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
-Weapon::Weapon(std::string type) : type(type)
+Weapon::Weapon(std::string const &type) : type(type)
 {
 	return;
 }
@@ -22,12 +22,12 @@ Weapon::~Weapon(void)
 	return;
 }
 
-std::string Weapon::getType(void) const
+std::string const &Weapon::getType(void) const
 {
 	return this->type;
 }
 
-void Weapon::setType(std::string type)
+void Weapon::setType(std::string const &type)
 {
 	this->type = type;
 
